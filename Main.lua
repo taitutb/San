b9a51b1e0848cb9744176f19cb13a0328a1efe8c
zozozo <115965333+taitutb@users.noreply.github.com>
2025-04-28 14:46:27 +0700

    Update and rename Main to Main.lua

---
+++ Main.lua
@@ -0,0 +1,11 @@
+function TypeGame(api)
+    if api.typeValidate == 0 then
+        print("typeValidate: " .. api.typeValidate)
+    elseif api.typeValidate == 1 then
+        print("typeValidate: " .. api.typeValidate)
+    elseif api.typeValidate == 2 then
+        print("typeValidate: " .. api.typeValidate)
+    else
+        print("typeValidate: Unknown (" .. api.typeValidate .. ")")
+    end
+end 
