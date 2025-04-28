d38460a84e85fe447b6fa792468ffbeef4de220e
zozozo <115965333+taitutb@users.noreply.github.com>
2025-04-28 09:51:13 +0700

    Update GameLogic.lua

--- GameLogic.lua
+++ GameLogic.lua
@@ -1,8 +1,11 @@
-print("Hello from Lua script!")
-
 function add(a, b)
     return a + b
 end
 
-local result = add(3, 4)
-print("Result of add(3, 4) = " .. result)
+function sub(a, b)
+    return a - b
+end
+
+function mul(a, b)
+    return a * b
+end
